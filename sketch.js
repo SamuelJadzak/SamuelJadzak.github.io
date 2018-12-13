4f8c0f3eeeb4325aa61b8261eb4e1b17a2df904d
sam jadzak <jadzak60636@gmail.com>
2018-12-13 15:38:52 -0800

    js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+
+  createCanvas(640, 480);
+}
+
+function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      if (pmouseX != mouseX) {
+	 fill( random(0,256),random(0,256),random(0,256) );
+      }
+    } 
+    rect(mouseX, mouseY, 80, 80);
+}
